2564dc6e30ad1a3231b714b7ee41553c84d0f445
Aung Kaung Myat <103877301+AungKaung1928@users.noreply.github.com>
2025-12-05 17:26:15 +0900

    Refactor general_example function and main logic

--- 01_helloworld/helloworld.cpp
+++ 01_helloworld/helloworld.cpp
@@ -3,33 +3,29 @@
 #include <vector>
 #include <string>
 using namespace std;
-
 void general_example() {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-    for (const string& word : msg) {
-        cout << word << " ";
-    }
-    cout << endl;
+  vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+  for (const string& word : msg) {
+    cout << word << " ";
+  }
+  cout << endl;
 }
-
 // ===== ROS2 EXAMPLE =====
 #include "rclcpp/rclcpp.hpp"
-
 class HelloNode : public rclcpp::Node {
 public:
-    HelloNode() : Node("hello_node") {
-        RCLCPP_INFO(this->get_logger(), "Hello ROS2 from C++!");
-    }
+  HelloNode() : Node("hello_node") {
+    RCLCPP_INFO(this->get_logger(), "Hello ROS2 from C++!");
+  }
 };
-
 int main(int argc, char **argv) {
-    // General C++
-    general_example();
-    
-    // ROS2
-    rclcpp::init(argc, argv);
-    auto node = std::make_shared<HelloNode>();
-    rclcpp::spin(node);
-    rclcpp::shutdown();
-    return 0;
+  // General C++
+  general_example();
+  
+  // ROS2
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<HelloNode>();
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+  return 0;
 }
